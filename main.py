c6fc6efb0c150dff474519edd38cbb87a62b6f96
rafaelbaptista <baprafael@icloud.com>
2024-01-22 03:25:34 +0100

    Setting up the project

---
+++ main.py
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------------------------------------- #
+# TATIA, Final Project - Emojislation                                                                      #
+# -------------------------------------------------------------------------------------------------------- #
+# 30 Jan 2024, Université Côte d'Azur.                                                                     #
+# Charafeddine Achir & Rafael Baptista.                                                                    #
+# -------------------------------------------------------------------------------------------------------- #
+
+import numpy as np
+import emoji
+import spacy
+import matplotlib.pyplot as plt
+from IPython.core.display import display, HTML
+import word2emoji
+from emo_uni import emo_list,emo_get
+from tqdm import tqdm
+from numpy import dot
+from numpy.linalg import norm
+
