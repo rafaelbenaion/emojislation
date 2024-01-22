0fe22a6c9d1dc82781cba37abd5f75e11b8b2da4
rafaelbaptista <baprafael@icloud.com>
2024-01-22 16:55:43 +0100

    Setting up Flask server for validation

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask
+import json
+import csv
+import string
+from flask import request
+from flask import jsonify
+from flask import Flask
+from flask import session
+from flask import render_template
+from pymongo import MongoClient
+from datetime import datetime
 app = Flask(__name__)
+
+client           = MongoClient("mongodb+srv://admin:admin@waterbnb.lo1mkvx.mongodb.net/")
+dbname           = 'WaterBnB'
+dbnames          = client.list_database_names()
+db               = client.WaterBnB
+collname         = 'emojis'
+collEmojislation = db.emojislation
+
 
 @app.route('/')
 def hello_world():
+
+
+    collEmojislation.insert_one({"userid": "ui"})
+
     return 'Hello, World!'
