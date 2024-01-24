b7ae051bb23230bdbbc695f559e41d193db7fdb1
rafaelbaptista <baprafael@icloud.com>
2024-01-24 20:03:45 +0100

    Setting up translation page and validation page

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ collEmojislation = db.emojislation
 @app.route('/')
 def hello_world():
 
-    return 'Hello, World!'
+    return render_template('index.html') # Render validation form
 
 @app.route('/validation')
 def validation():
